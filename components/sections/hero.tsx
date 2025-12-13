b1542b4d5fce27858a2e8ea8f9374d88afe04989
Marek <cmelo.marek@gmail.com>
2025-12-13 03:43:52 +0100

    Fix: Zväčšená medzera medzi Kontakt a scroll indikátorom - paddingBottom 100px

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -249,7 +249,7 @@ export function Hero() {
               paddingTop: "0",
               paddingLeft: "0",
               paddingRight: "0",
-              paddingBottom: "60px",
+              paddingBottom: "100px",
               boxSizing: "border-box",
               position: "relative",
             }}
